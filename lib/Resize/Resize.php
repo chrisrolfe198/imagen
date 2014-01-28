cf90a1789b2a2da571bdf1f1fa0650bd8d8376bd
Christopher Rolfe <christopher.rolfe198@gmail.com>
2014-01-28 21:15:48 +0000

    Adding some fleshing out of the resize class

--- lib/Resize/Resize.php
+++ lib/Resize/Resize.php
@@ -4,5 +4,28 @@ namespace ThatChrisR\Imagen\Resize;
 
 class Resize
 {
+	public function image(BaseImage $image)
+	{
+		$this->image = $image;
+	}
 
+	public function width($width)
+	{
+		$this->width = $width;
+	}
+
+	public function height($height)
+	{
+		$this->height = $height;
+	}
+
+	public function resize()
+	{
+		if (($this->height) and ($this->width)) {
+			$newHeight = $this->height;
+			$newWidth = $this->width;
+		}
+	}
 }
+
+// Resize->image($resource)->width('300')->resize();
