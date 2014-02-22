69e5dc56aaa82684ab6b10b095e1c38a36169fb2
Christopher Rolfe <christopher.rolfe198@gmail.com>
2014-02-22 02:25:24 +0000

    Adding a basic text overlay class

---
+++ lib/Overlay/TextOverlay.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Overlays text
+ *
+ * @package \ThatChrisR\Imagen
+ * @author Christopher Rolfe christopher.rolfe198@gmail.com
+ */
+
+namespace ThatChrisR\Imagen\Overlay;
+
+/**
+ * Handles overlaying text
+ */
+class TextOverlay
+{
+}
