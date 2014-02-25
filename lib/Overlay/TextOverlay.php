91af64d219ca44c169de50f0ae25c6a1587d087a
Christopher Rolfe <christopher.rolfe198@gmail.com>
2014-02-25 05:17:06 +0000

    Creating the add text method and handling it so that colours are created properly - needs some refactoring further Ignoring ttf files

--- lib/Overlay/TextOverlay.php
+++ lib/Overlay/TextOverlay.php
@@ -8,11 +8,18 @@
 
 namespace ThatChrisR\Imagen\Overlay;
 
+use ThatChrisR\Imagen\Base\BaseImage;
+
 /**
  * Handles overlaying text
  */
 class TextOverlay
 {
+	/**
+	 * Holds the image to manipulate, must be an instance of BaseImage
+	 */
+	public $_image;
+
 	/**
 	 * An array to handle fonts
 	 */
@@ -23,17 +30,56 @@ class TextOverlay
 	 */
 	public static $colours = array();
 
+	public function __construct(BaseImage $image)
+	{
+		$this->_image = $image->get_image_resource();
+	}
+
 	/**
 	 * Adds a font to the fonts array
 	 */
-	public static function add_font($font) {
-		array_push(self::$fonts, $font);
+	public static function add_font($font_name, $font_path)
+	{
+		self::$fonts[$font_name] = $font_path;
 	}
 
 	/**
 	 * Adds a colour to the colours array
 	 */
-	public static function add_colour($colour) {
-		array_push(self::$colours, $colour);
+	public static function add_colour($colour_name, $colour_path)
+	{
+		self::$colours[$colour_name] = $colour_path;
+	}
+
+	/**
+	 * Adds text to an image
+	 * @todo migrate the creation of a colour resource out of the function
+	 */
+	public function add_text($text, $colour_name, $font_name, $size = 16)
+	{
+		$colour = self::$colours[$colour_name];
+
+		if (is_string($colour)) {
+			self::$colours[$colour_name] = $this->_create_hex_colour_resource($colour);
+		}
+
+		imagettftext(
+			$this->_image,					// Image resource
+			$size,							// Font size
+			0,								// Font Angle
+			30,								// X position
+			30,								// Y position
+			self::$colours[$colour_name],	// Colour to use from the array
+			self::$fonts[$font_name],		// Font to use from array
+			$text 							// Text to add
+			);
+		return $this->_image;
+	}
+
+	protected function _create_hex_colour_resource($colour) {
+		$first = intval(substr($colour, 0, 2));
+		$second = intval(substr($colour, 2, 2));
+		$third = intval(substr($colour, 4, 2));
+		return imagecolorallocate($this->_image, $first, $second, $third);
 	}
 }
