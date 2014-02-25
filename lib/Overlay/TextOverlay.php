0d97a076c77782e6e3608a4f0713a777b3cf074d
Christopher Rolfe <christopher.rolfe198@gmail.com>
2014-02-25 04:50:37 +0000

    Adding the structure too hold multiple fonts and colours

--- lib/Overlay/TextOverlay.php
+++ lib/Overlay/TextOverlay.php
@@ -13,4 +13,27 @@ namespace ThatChrisR\Imagen\Overlay;
  */
 class TextOverlay
 {
+	/**
+	 * An array to handle fonts
+	 */
+	public static $fonts = array();
+
+	/**
+	 * An array to handle the text colours
+	 */
+	public static $colours = array();
+
+	/**
+	 * Adds a font to the fonts array
+	 */
+	public static function add_font($font) {
+		array_push(self::$fonts, $font);
+	}
+
+	/**
+	 * Adds a colour to the colours array
+	 */
+	public static function add_colour($colour) {
+		array_push(self::$colours, $colour);
+	}
 }
