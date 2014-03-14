f52834e3108135bba673ce985897c726821deb5e
Christopher Rolfe <christopher.rolfe198@gmail.com>
2014-03-14 08:21:31 +0000

    Adding the ability to add a stroke to text (needs work)

--- lib/Overlay/TextOverlay.php
+++ lib/Overlay/TextOverlay.php
@@ -31,6 +31,8 @@ class TextOverlay
 	 */
 	public static $colours = array();
 
+	public static $stroke = array();
+
 	public function __construct(BaseImage $image)
 	{
 		$this->image = $image->get_image_resource();
@@ -47,9 +49,10 @@ class TextOverlay
 	/**
 	 * Adds a colour to the colours array
 	 */
-	public static function add_colour($colour_name, $colour_path)
+	public static function add_colour($colour_name, $colour_path, $strokecolor)
 	{
 		self::$colours[$colour_name] = $colour_path;
+		self::$stroke[$colour_name] = $strokecolor;
 	}
 
 	/**
@@ -58,27 +61,34 @@ class TextOverlay
 	 * @todo Set the xpos and ypos to be done based on percentages
 	 * @todo Once the todo to migrate the image property to being an instance of BaseImage is done imagesx/y should be removed
 	 */
-	public function add_text($text, $colour_name, $font_name, $xpos = 0, $ypos = 0, $size = 16)
+	public function add_text($text, $colour_name, $font_name, $xpos = 0, $ypos = 0, $size = 16, $strokecolor = false, $stroke = 0)
 	{
+
+		if(!$strokecolor) {
+			$strokecolor = $colour_name;
+		}
 
 		$xpos = $this->_calculate_percentage(imagesx($this->image), $xpos);
 		$ypos = $this->_calculate_percentage(imagesy($this->image), $ypos, $size);
 
-		$colour = self::$colours[$colour_name];
-
-		if (is_string($colour)) {
-			self::$colours[$colour_name] = $this->_create_colour_resource($colour);
+		if (is_string(self::$colours[$colour_name])) {
+			self::$colours[$colour_name] = $this->_create_colour_resource(self::$colours[$colour_name]);
+		}
+		if (is_string(self::$stroke[$colour_name])) {
+			self::$stroke[$colour_name] = $this->_create_colour_resource(self::$stroke[$colour_name]);
 		}
 
-		imagettftext(
+		imagettfstroketext(
 			$this->image,					// Image resource
 			$size,							// Font size
 			0,								// Font Angle
 			$xpos,							// X position
 			$ypos,							// Y position
 			self::$colours[$colour_name],	// Colour to use from the array
+			self::$stroke[$colour_name],	// Colour to use from the array
 			self::$fonts[$font_name],		// Font to use from array
-			$text 							// Text to add
+			$text, 							// Text to add
+			$stroke 
 			);
 		return $this->image;
 	}
